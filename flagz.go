8661e8eb156418c574e585ec65b65986a9e43794
chaaaaaaaaaaaaaaaaaaase <chase@loopreturns.com>
2021-10-22 14:50:57 -0400

    added post-parse flag getters, tests, updated example

--- flagz.go
+++ flagz.go
@@ -1,32 +1,34 @@
 package flagz
 
 import (
+	"flag"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 type Flagz []string
 
-func (s *Flagz) String() string {
-	return strings.Join(s.Array(), ", ")
+func (f *Flagz) String() string {
+	return strings.Join(f.Array(), ", ")
 }
 
-func (s *Flagz) Set(str string) error {
-	*s = append(*s, str)
+func (f *Flagz) Set(str string) error {
+	*f = append(*f, str)
 
 	return nil
 }
 
-func (s *Flagz) Array() []string {
-	return *s
+func (f *Flagz) Array() []string {
+	return *f
 }
 
-func (s *Flagz) Stringz() []string {
-	return s.Array()
+func (f *Flagz) Stringz() []string {
+	return f.Array()
 }
 
-func (s *Flagz) Intz() ([]int, error) {
-	strs := s.Stringz()
+func (f *Flagz) Intz() ([]int, error) {
+	strs := f.Stringz()
 	ints := make([]int, len(strs))
 
 	for index, str := range strs {
@@ -42,8 +44,8 @@ func (s *Flagz) Intz() ([]int, error) {
 	return ints, nil
 }
 
-func (s *Flagz) Boolz() ([]bool, error) {
-	strs := s.Stringz()
+func (f *Flagz) Boolz() ([]bool, error) {
+	strs := f.Stringz()
 	boolz := make([]bool, len(strs))
 
 	for index, str := range strs {
@@ -59,8 +61,8 @@ func (s *Flagz) Boolz() ([]bool, error) {
 	return boolz, nil
 }
 
-func (s *Flagz) Floatz() ([]float64, error) {
-	strs := s.Stringz()
+func (f *Flagz) Floatz() ([]float64, error) {
+	strs := f.Stringz()
 	floatz := make([]float64, len(strs))
 
 	for index, str := range strs {
@@ -76,4 +78,92 @@ func (s *Flagz) Floatz() ([]float64, error) {
 	return floatz, nil
 }
 
+func lookup(name string) (interface{}, error) {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
 
+	option := flag.Lookup(name)
+
+	if option == nil {
+		return nil, fmt.Errorf("failed to find flag %s", name)
+	}
+
+	value := option.Value
+
+	if value == nil {
+		return nil, fmt.Errorf("failed to find value for flag %s", name)
+	}
+
+	getter, ok := value.(flag.Getter)
+
+	if !ok {
+		return nil, fmt.Errorf("failed to convert value to getter for flag %s", name)
+	}
+
+	return getter.Get(), nil
+}
+
+func String(name string) (string, error) {
+	val, err := lookup(name)
+
+	if err != nil {
+		return "", err
+	}
+
+	str, ok := val.(string)
+
+	if !ok {
+		err = fmt.Errorf("invalid type for flag %s", name)
+	}
+
+	return str, err
+}
+
+func Int(name string) (int, error) {
+	val, err := lookup(name)
+
+	if err != nil {
+		return 0, err
+	}
+
+	i, ok := val.(int)
+
+	if !ok {
+		err = fmt.Errorf("invalid type for flag %s", name)
+	}
+
+	return i, err
+}
+
+func Bool(name string) (bool, error) {
+	val, err := lookup(name)
+
+	if err != nil {
+		return false, err
+	}
+
+	b, ok := val.(bool)
+
+	if !ok {
+		err = fmt.Errorf("invalid type for flag %s", name)
+	}
+
+	return b, err
+}
+
+func Float(name string) (float64, error) {
+	val, err := lookup(name)
+
+	if err != nil {
+		return 0, err
+	}
+
+	f, ok := val.(float64)
+
+	if !ok {
+		err = fmt.Errorf("invalid type for flag %s", name)
+	}
+
+	return f, err
+}
